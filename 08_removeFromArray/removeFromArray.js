be4f777be852e8cca3487d6dfe6153d6a632389a
Garychamp <garyhoare18@gmail.com>
2026-01-30 16:57:05 +0000

    Updated Javascript files to complete the exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    for (let item of arr) {
+        if (!args.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
 };
+console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+        return 'Error';
+    }
+
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let total = 0;
+
+    for (let i = start; i <= end; i++) {
+    total += i;
+    }
+
+    return total;
 
 };
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
