8bbc0973031b62745d73f235c28351ad2e965e29
Garychamp <garyhoare18@gmail.com>
2026-02-21 12:47:48 +0000

    Refactor findTheOldest function to accept people parameter and calculate the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  function getAge(person) {
+    const death = person.yearOfDeath || new Date().getFullYear();
+    return death - person.yearOfBirth;
+  }
 
+  const oldest = people.reduce(function(prev, current) {
+    return getAge(prev) > getAge(current) ? prev : current;
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
